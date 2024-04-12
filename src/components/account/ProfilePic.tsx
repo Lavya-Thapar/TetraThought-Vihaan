08c10f85678e041c5e093013c945f3ae117c72cd
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2024-04-13 00:27:47 +0530

    user info

---
+++ src/components/account/ProfilePic.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { User } from "@/types/user";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function ProfilePic() {
+  const { data: session } = useSession();
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    if (session && session.user && session.user.email && session.user.image) {
+      const account: User = {
+        email: session.user.email,
+        name: "Manikya",
+        photo: session.user.image,
+        photoBase64: false,
+        achievements: ["Completed deadline 1", "Completed deadline 2"],
+      };
+      setUser(account);
+    }
+  }, [session]);
+  return (
+    <div>
+      {user && user.photoBase64 ? (
+        <Image
+          src={`data:image/png;base64,${user.photo}`}
+          referrerPolicy="no-referrer"
+          alt=""
+          width={260}
+          height={260}
+        />
+      ) : (
+        user?.photo && (
+          <Image
+            src={user.photo}
+            referrerPolicy="no-referrer"
+            alt=""
+            width={260}
+            height={260}
+          />
+        )
+      )}
+    </div>
+  );
+}
---
+++ src/types/user.d.ts
@@ -0,0 +1,7 @@
+export type User = {
+  email: string;
+  photo: string;
+  name: string;
+  photoBase64?: bool;
+  achievements: Array<string>;
+};
