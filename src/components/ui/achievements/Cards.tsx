4619c97a233297d1c8cf89603982ba7a57baf300
HarshS490 <142019268+HarshS490@users.noreply.github.com>
2024-04-13 01:35:40 +0530

    achievements page

---
+++ src/components/ui/achievements/Cards.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    title:string,
+    description:string,
+    date:Date,
+}
+
+export default function Cards({}: Props) {
+  return (
+    <div>Cards</div>
+  )
+}
